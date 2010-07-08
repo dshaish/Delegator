ec3b4f7ef7c07b0bdc9d8570f33ced193fe8981a
Dor Shaish <dorshaish@gmail.com>
2010-07-09 00:23:20 +0300

    Added Login module and Authentication.
    Added UserTaskFilter Enum.

--- src/org/delegator/engine/TasksList.java
+++ src/org/delegator/engine/TasksList.java
@@ -1,24 +1,19 @@
 package org.delegator.engine;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.delegator.entities.Tasks;
 
 class TasksList {
 	
-	private static final String EJBQL = "select tasks from Tasks tasks";
-
-	private static final String[] RESTRICTIONS = {
-			"lower(tasks.title) like concat(lower(#{tasksList.tasks.title}),'%')",
-			"lower(tasks.description) like concat(lower(#{tasksList.tasks.description}),'%')",
-			"lower(tasks.attachment) like concat(lower(#{tasksList.tasks.attachment}),'%')",
-			"lower(tasks.status) like concat(lower(#{tasksList.tasks.status}),'%')", };
-
-	private Tasks tasks = new Tasks();
+	List<Tasks> taskList = new LinkedList<Tasks>();
 
 	public TasksList() {
 	}
 
 	public Tasks getTasks() {
-		return tasks;
+		return null;
 	}
 	
 	
